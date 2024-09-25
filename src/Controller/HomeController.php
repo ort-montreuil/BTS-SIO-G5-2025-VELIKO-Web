630eb4affd610f2b6d6240beea0699d6e0a9e108
Noam <noam.baroukh.pro@gmail.com>
2024-09-25 15:20:59 +0200

    le demarrage sera directement dans la page Home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/', name: 'app_home')]
 
     public function execute(Request $request): Response
     {
