31205c560f02b02eaef9256e9db8ac3f1d175cfc
Aedery16-11 <aedery.16.11@gmail.com>
2024-09-29 11:39:48 +0200

    Ajout d'une barre de navigation, de package Version 1 terminée

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
@@ -19,7 +20,7 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username', TextType::class)
+            ->add('email', EmailType::class)
             ->add('nom', TextType::class)  // Add the 'nom' field here
             ->add('prenom', TextType::class)
             ->add('date_naissance', DateType::class, [
