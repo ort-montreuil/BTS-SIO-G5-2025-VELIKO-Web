2952463af78b04b02daefe05ae6727d25d705946
Noam <noam.baroukh.pro@gmail.com>
2024-10-11 09:29:28 +0200

    mes station controller test

--- src/Controller/MesStationsController.php
+++ src/Controller/MesStationsController.php
@@ -2,36 +2,35 @@
 
 namespace App\Controller;
 
-use App\Controller\StationFavorisController;
 use App\Entity\Station;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Component\Routing\Annotation\Route;
 
 class MesStationsController extends AbstractController
 {
-    private HttpClientInterface $client;
     private EntityManagerInterface $entityManager;
 
-    public function __construct(HttpClientInterface $client, EntityManagerInterface $entityManager)
+    public function __construct(EntityManagerInterface $entityManager)
     {
-        $this->client = $client;
         $this->entityManager = $entityManager;
     }
 
     #[Route('/mes/stations', name: 'app_mes_stations')]
     public function index(): Response
     {
-        $response = $this->client->request('GET', 'http://localhost:9042/api/stations');
-        $stations = $response->toArray();
+        // Récupérer l'utilisateur connecté
+        $user = $this->getUser();
+
+        // Récupérer les stations favorites de l'utilisateur depuis la base de données
+        $stationsRepository = $this->entityManager->getRepository(Station::class);
+        $userStations = $stationsRepository->findBy(['emailuser' => $user->getEmail()]);
 
         return $this->render('mes_stations/index.html.twig', [
             'controller_name' => 'MesStationsController',
-            'stations' => $stations,
+            'stations' => $userStations,
         ]);
     }
 }
